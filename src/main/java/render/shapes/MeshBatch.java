9440379c2cc92a2b65c79ae399b73fe9aafdfbe0
Bxkos20 <jruabescos@proton.me>
2026-03-02 13:08:10 +0100

    MeshBatch create

---
+++ src/main/java/render/shapes/MeshBatch.java
@@ -0,0 +1,16 @@
+package render.shapes;
+
+import com.badlogic.gdx.graphics.Mesh;
+import render.shapes.rawDataMesh.RawDataMesh;
+
+import java.util.ArrayList;
+
+public class MeshBatch {
+    private Mesh mesh;
+    private int lastVertex;
+    private int lastIndex;
+
+    public void add (RawDataMesh rawDataMesh, float x, float y){
+        mesh.setVertices()
+    }
+}
